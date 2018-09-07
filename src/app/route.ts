a53d9c9d0f0110b796829be7493e10a6e4961d04
Alfred hitch cock <aurelien.binet34@gmail.com>
2018-09-07 15:14:36 +0200

    une vie de route

--- src/app/route.ts
+++ src/app/route.ts
@@ -3,10 +3,21 @@ import {ClientComponent} from './client/client.component';
 import {HomeComponent} from './home/home.component';
 import {ReservationComponent} from './reservation/reservation.component';
 import {PassagerComponent} from './passager/passager.component';
+import {ReservationEditComponent} from './reservation/reservation-edit.component';
+import {ClientEditComponent} from './client/client-edit.component';
 
 export const routes: Routes = [
  {path: 'reservation', component: ReservationComponent},
+  {path: 'reservation/create', component: ReservationEditComponent},
+  {path: 'reservation/edit/:id', component: ReservationEditComponent},
   {path: 'home', component: HomeComponent},
  {path: 'passager', component: PassagerComponent},
-  {path: 'client', component: ClientComponent}
+ // {path: 'passager/create', component: PassagerEditComponent},
+ // {path: 'passager/edit/:id', component: PassagerEditComponent},
+  {path: 'client', component: ClientComponent},
+  {path: 'client/create', component: ClientEditComponent},
+  {path: 'client/edit/:id', component: ClientEditComponent},
+  {path: 'vol', component: VolComponent},
+  {path: 'vol/create', component: VolEditComponent},
+  {path: 'vol/edit/:id', component: VolEditComponent},
 ];
