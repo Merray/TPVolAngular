f9ad32be726f760b246e61de66cc0ef4d919b20c
Guilitane <jonvivlau@hotmail.fr>
2018-09-07 11:50:34 +0200

    creation du restController passager

--- src/app/model/passager.ts
+++ src/app/model/passager.ts
@@ -1,2 +1,37 @@
 export class Passager {
+  constructor(private _idPassager?: number, private _nom?: string, private _prenom?: string, private _reservations: Reservation[]) {
+
+  }
+
+  get idPassager(): number {
+    return this._idPassager;
+  }
+
+  set idPassager(value: number) {
+    this._idPassager = value;
+  }
+
+  get nom(): string {
+    return this._nom;
+  }
+
+  set nom(value: string) {
+    this._nom = value;
+  }
+
+  get prenom(): string {
+    return this._prenom;
+  }
+
+  set prenom(value: string) {
+    this._prenom = value;
+  }
+
+  get reservations(): Reservation[] {
+    return this._reservations;
+  }
+
+  set reservations(value: Reservation[]) {
+    this._reservations = value;
+  }
 }
