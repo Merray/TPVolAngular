3262fd2c7effd20bdf73d29b231e751691aa3ea4
Guilitane <jonvivlau@hotmail.fr>
2018-09-07 10:52:06 +0200

    ajout des folders model et service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PassagerComponent } from './service/passager/passager.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PassagerComponent
   ],
   imports: [
     BrowserModule
