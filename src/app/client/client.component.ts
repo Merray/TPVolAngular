fbc6e91e5010cf508e4c321e4c2d44cac95fe3d5
Alfred hitch cock <aurelien.binet34@gmail.com>
2018-09-07 15:03:45 +0200

    service rendu

--- src/app/client/client.component.ts
+++ src/app/client/client.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Client} from '../model/client';
+import {ClientService} from '../service/client/client.service';
 
 @Component({
   selector: 'app-client',
@@ -10,10 +11,18 @@ export class ClientComponent implements OnInit {
 
   clients: Client[] = [new Client(21, 'fricelle')];
 
-  constructor() {
+  constructor(private clientService: ClientService) {
   }
 
   ngOnInit() {
+    this.list();
   }
+
+  public list() {
+    this.clientService.list().subscribe(resp => {
+      this.clients = resp;
+    }, error => console.log(error));
+  }
+
 
 }
--- src/app/model/client-moral.ts
+++ src/app/model/client-moral.ts
@@ -1,2 +1,14 @@
-export class ClientMoral {
+import {Client} from './client';
+
+export class ClientMoral extends Client {
+  get siret(): string {
+    return this._siret;
+  }
+
+  set siret(value: string) {
+    this._siret = value;
+  }
+  constructor(private _siret: string) {
+    super();
+  }
 }
