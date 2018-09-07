2dec924d6096b7ec1bcadb20dfc633ed9197582b
Alfred hitch cock <aurelien.binet34@gmail.com>
2018-09-07 10:56:47 +0200

    création des routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {routes} from './route';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
